146b0326f0cb5275950754745ec2ebdf85443f29
Ashutosh Tiwari <97720235+InCoderWeb@users.noreply.github.com>
2024-01-02 01:17:11 +0530

    latest update

---
+++ src/app/layout.tsx
@@ -0,0 +1,38 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/ReactToastify.min.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+	title: "Quiz App - InCoderWeb",
+	description: "This is a quiz application created by InCoderWeb.",
+};
+
+export default function RootLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	return (
+		<html lang="en">
+			<body className={inter.className}>
+				{children}
+				<ToastContainer
+					position="top-right"
+					autoClose={5000}
+					hideProgressBar={false}
+					newestOnTop={false}
+					closeOnClick
+					rtl={false}
+					pauseOnFocusLoss
+					draggable
+					pauseOnHover
+					theme="light"
+				/>
+			</body>
+		</html>
+	);
+}
---
+++ src/categories.ts
@@ -0,0 +1,50 @@
+export let categories = [
+    {
+        name: "Geography",
+        imageURL: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEh7LlL0MSt-xz87OIqGfpDp9RzD7I7TUZAcxp5pJjkRry2b_PiUM5pYOLQAllpnbPLPraEgurrQ9yh90BR1f23fPCBAxYy5Qs88R-Zj68Fb8EYOjaH4FNaYNMljSd5OUFjJZZ1G8b5O8uazd2ccRjGzQTOeWFP6gIMTVkxuJ2i0z7HjNcfC3MDOnctWLgPT/s320/pexels-aaditya-arora-592753.jpg",
+    },
+    {
+        name: "History",
+        imageURL: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgy9piE49el8JzS8SiOCw4NJIpZgh7iy0ks_2tufkc-U4xbTmW_sPuBqOfMl23mGZhq8ovfg0hGuWfXt63n6bS1F-G0atgrMlmFJxG2Gtp8KrgWCE7tW8duKS4XNZkXqmUudV5jLFTDkJPn_wfSDM_AxcPP4XGF-5CLdZVVM46dTyTIyDRExsBYrEuVimHU/s320/pexels-pixabay-36006.jpg",
+    },
+    {
+        name: "Science",
+        imageURL: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjniMYaTMpCBvkMYNDgGDoTnaxtaDzJcJXg_DpD_uEInsnDJfDo_LFTfNTnXgGjqb1sMab68eopoPXNluEJfjpKSaPlin8zo4z90kZVK_0fvzBCwRqRlTD9hyphenhyphenX4NvHXxNSWT3TEiAHGbCatEx-rzv9E5vqod0Qll-1Hk8zlSJ8fZHVkqRuQrGTfvWcpW6u1/s320/pexels-pixabay-2150.jpg",
+    },
+    {
+        name: "Current Affairs",
+        imageURL: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgl1bm-KDu_andLi2t-jFTW0pY8V3Wgk4n5BDpGB4if3ahndnZrPKaSSdcksxPUcqywahkxzH4-G-JTxnsMa4pzS4SXF3gdWpTdn8_DHYYS4uFO7J9bOHyeERHCTtdLYcI_1ABxKL8yZnlIslVyNasSw24EzKEoZqxERJUTK-TP7hqeelPo-HNUePvB7r8_/s320/pexels-pixabay-459728.jpg",
+    },
+    {
+        name: "Politics",
+        imageURL: "https://blogger.googleusercontent.com/img/a/AVvXsEiAtjFLyQ1mD0rJZ4cKfHEgDclLsEtZ80lKsAwfyIaWb8XIk_d0t9AeP93Thnyj1_ZTH4OdjatSUw6nGVd1Z54PXI21U-RuGveWPPRbmvO4LU7o9Dt9GwLu-TotKznAbn2248G68vqSmMcd0Hu-m5pZ2sJDQAQ5jhrx-6ynUDMYGk2swCWL0sHGObqmiHhH",
+    },
+    {
+        name: "Movies",
+        imageURL: "https://blogger.googleusercontent.com/img/a/AVvXsEjoi5uO5pjrrb2WYdOyXosXyVtHgSz6dcnEFpl5qAT38UPAuOBjnVQTAcPcJIOlbzlbTwgDjrsBsZdPkzkSoNY4NGC_KwMUNbWYvzLqJJ0tVgdFZcv0042cbSvNmz7TiAn3rUoemvy6_5EXjsnrUe2UM7DrfX8L3Synt6qLl9EvoiqoO0PKyjl21WpEBgoE",
+    },
+    {
+        name: "Music",
+        imageURL: "https://blogger.googleusercontent.com/img/a/AVvXsEiDn-OYKLm4Y8VkstgzxD09zBThEJ39IHSQD5fYQmCI_DqNC7GjffkdDSROw-2Mnok5bgYIEB2LRt061772JpBMfAvWphnIrfRow-MfBeMaCRf-5zWNSiOQMmgHWSRHhm5-ZdGPu47IgaQSA1NpweVib-I4UlnJHKo51iftxlTlKCYQwMM4l_I5Xbaig-aY",
+    },
+    {
+        name: "Celebrities",
+        imageURL: "https://blogger.googleusercontent.com/img/a/AVvXsEghUHyJZ7TisaR_iehetScq-6p1bq2pSTAtD-yTYflVJ0EV6oPiA0LePB3R9lfgrSP_aPcRl7whamMFAMGW4FlNw-3OWl37ruseAdfcOWW13tquoVqDXAH3aiRoHcnGmla32cFmT_8SsDct92lP7y1enO8PpKl2JltKKnxAZvC2SurupCnb9mDVHIbo5pSw",
+    },
+    {
+        name: "Soccer",
+        imageURL: "https://blogger.googleusercontent.com/img/a/AVvXsEj4enzx_Nyto2rN5u1VnrDWuNDNY5F7DGlOLAVqAkG_G1aXmDZSKFs-vLUa-c6gt90IF0ixF0DtCNZYtWDRXLYDD2DYCv6DsY3EiDaHg8HonWSwKJrbPCuLXx_WLXeB8702AVFqMOcLX8oVgygbyRJSTrKgM_ACnG5H8HmojqyY1kcBBce-9Szi3kwSa8wa",
+    },
+    {
+        name: "Internet",
+        imageURL: "https://blogger.googleusercontent.com/img/a/AVvXsEguH6lGj_BqPxJvBGRzGJ3avi_tJMzwtsjknWn1JHPnTchNLCM247tFHkqWpN1Mq8J7A9vyYYGe7e6PVF0gxWQbHBRqvk7nzl6a1Fw_vPx6wv3ULUMLeS3ipNlPqYU9nY9ECQTVHeYo7a2wMxsN5U2fpeutJGpWQ7D78eDY7Rla8dc2vV0vBKg0PNAPt6oz",
+    },
+    {
+        name: "Computers",
+        imageURL: "https://blogger.googleusercontent.com/img/a/AVvXsEhbKn3862TF5dN_y4xZg9W1OTZSinDU_oMOUJcIZuOIUmgPxGWKaA1kJGJtMJYJ2CfII2aoNPltQqIjh2hGBA-MGlT2I4gMfLoTJE3PwqwrSgWmAOZMmJk62iiXD-M06DwG7ZX__FooQeQ0IkCyZNGrx61-fjVLp3OLHK2cBSaEVLtaaZd2lZ2YxI9mhBrw",
+    },
+    {
+        name: "Tech History",
+        imageURL: "https://blogger.googleusercontent.com/img/a/AVvXsEgt51c6qB8RxwDocxKVREkTPap2tXl_ZuykRGyI_L5RDdwb0BySvRExPOQf-iPaT6O_l8ozs-bg1-ShNTy0JyyHKuDVO4GLs44lcqOSG9QzthzjB90Ev_2PN8xf4i6du-YJBXkz5mWExjDPcHaptk71XFtTOEIiQdhG91fQ9dNeJpMlJYj1LYCsJP3XhPzz",
+    },
+]
